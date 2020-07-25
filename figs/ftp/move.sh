104166f59ea0449ef72740ed1f424755791fea35
Nick Amin <amin.nj@gmail.com>
2020-07-24 22:13:40 -0700

    add ss results

---
+++ figs/ftp/move.sh
@@ -0,0 +1,25 @@
+mv Figure_002-a.pdf sr_njets_prefit.pdf    
+mv Figure_002-b.pdf sr_nbtags_prefit.pdf    
+mv Figure_002-c.pdf sr_ht_prefit.pdf    
+mv Figure_002-d.pdf sr_met_prefit.pdf    
+
+mv Figure_003-a.pdf ttwcr_njets_prefit.pdf    
+mv Figure_003-b.pdf ttwcr_nbtags_prefit.pdf    
+mv Figure_003-c.pdf ttzcr_njets_prefit.pdf    
+mv Figure_003-d.pdf ttzcr_nbtags_prefit.pdf    
+
+mv Figure_004-a.pdf SRCR_postfit.pdf    
+mv Figure_004-b.pdf SRDISC_postfit.pdf    
+
+mv Figure_005.pdf yukawa.pdf 
+mv Figure_006.pdf plot_2d_phizprime.pdf 
+
+mv Figure_007-a.pdf ft_higgs_sc_scan_limit.pdf 
+mv Figure_007-b.pdf ft_higgs_ps_scan_limit.pdf 
+
+mv Figure_008-a.pdf plot_2d_2hdm_tanbetaexclusion_h.pdf 
+mv Figure_008-b.pdf plot_2d_2hdm_tanbetaexclusion_a.pdf 
+mv Figure_008-c.pdf plot_2d_2hdm_tanbetaexclusion_b.pdf 
+
+mv Figure_009-a.pdf plot_2d_dmscalar_xsec_totsm_bothcouplings.pdf 
+mv Figure_009-b.pdf plot_2d_dmpseudo_xsec_totsm_bothcouplings.pdf 
